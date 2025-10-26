aa9419a807a88f047ecf51d4f68d23dfa9d5623b
jun562 <capboy290@naver.com>
2025-10-27 03:11:56 +0900

    feat(validator): 자동차 이름 비단어 문자 포함 여부 검증 로직 추가

--- src/main/java/racingcar/model/Validator.java
+++ src/main/java/racingcar/model/Validator.java
@@ -11,10 +11,13 @@ public class Validator {
 
     private static final int MIN_CAR_COUNT = 2;
     private static final int MAX_CAR_NAME_LENGTH = 5;
+    private static final String CAR_NAME_REGEX = "^[a-zA-Z0-9_가-힣]+$";
+
     private static final String ERROR_MESSAGE = "경주할 자동차는 2대 이상이어야 합니다.";
     private static final String ERROR_MESSAGE2 = "경주할 자동차의 이름은 중복되지 않아야 합니다.";
     private static final String ERROR_MESSAGE3 = "자동차의 이름은 비어있지 않아야 합니다.";
     private static final String ERROR_MESSAGE4 = "자동차의 이름은 5글자 이하여야 합니다.";
+    private static final String ERROR_MESSAGE5 = "자동차의 이름은 영문자, 한글, 숫자, 밑줄(_)의 조합이어야 합니다.";
 
     public void validate(List<String> carNames) {
         Set<String> carNamesSet = new HashSet<>(carNames);
@@ -30,7 +33,8 @@ public class Validator {
         if (carNames.stream().mapToInt(String::length).anyMatch(length -> length > 5)) {
             throw new IllegalArgumentException(ERROR_MESSAGE4);
         }
-
+        if (carNames.stream().anyMatch(carName -> !carName.matches(CAR_NAME_REGEX))) {
+            throw new IllegalArgumentException(ERROR_MESSAGE5);
+        }
     }
-
 }
