9aea0326b4c5426213af27e187cf64ef2a3cd1b1
jun562 <capboy290@naver.com>
2025-10-26 20:04:44 +0900

    refactor: parse 메서드 두가지 기능 분리

--- src/main/java/racingcar/model/Parser.java
+++ src/main/java/racingcar/model/Parser.java
@@ -10,11 +10,16 @@ public class Parser {
     }
 
     public List<String> parse(String input) {
-        List<String> carNames = List.of(input.split(","));
+        return trimCarNames(splitCarNames(input));
+    }
 
+    private List<String> splitCarNames(String input) {
+        return List.of(input.split(","));
+    }
+
+    private List<String> trimCarNames(List<String> carNames) {
         Stream<String> carNamesStream = carNames.stream();
         Stream<String> trimmedCarNamesStream = carNamesStream.map(String::trim);
-
         return trimmedCarNamesStream.collect(Collectors.toList());
     }
 }
