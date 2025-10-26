33b6f6d55c528846b8e7501a88580ba59a815c61
jun562 <capboy290@naver.com>
2025-10-26 20:02:50 +0900

    test(unit): 자동차 이름 분리 테스트 케이스 추가

--- src/test/java/racingcar/model/ParserTest.java
+++ src/test/java/racingcar/model/ParserTest.java
@@ -30,4 +30,36 @@ class ParserTest {
         List<String> result = parser.parse(input);
         assertEquals(result, List.of("car", "woni", "jun", "woowa"));
     }
+
+    @Test
+    @DisplayName("자동차_이름이_하나일_경우")
+    void parseCarNameSingle() {
+        String input = "car";
+        List<String> result = parser.parse(input);
+        assertEquals(result, List.of("car"));
+    }
+
+    @Test
+    @DisplayName("자동차_이름이_없는_경우")
+    void parseCarNamesWithEmptyItem() {
+        String input = "car,,wooni";
+        List<String> result = parser.parse(input);
+        assertEquals(result, List.of("car", "", "wooni"));
+    }
+
+    @Test
+    @DisplayName("입력이_쉼표로_끝나는_경우")
+    void parseCarNamesEndsWithComma() {
+        String input = "car,woni,";
+        List<String> result = parser.parse(input);
+        assertEquals(result, List.of("car", "woni"));
+    }
+
+    @Test
+    @DisplayName("입력이_빈_문자열인_경우")
+    void parseCarNamesWithEmpty() {
+        String input = "";
+        List<String> result = parser.parse(input);
+        assertEquals(result, List.of(""));
+    }
 }
