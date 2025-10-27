80b6595c80aa715267c2a7a40967750277f57840
jun562 <capboy290@naver.com>
2025-10-27 16:20:27 +0900

    feat(RacingGame): 0~9 사이의 무작위 값 생성 로직 추가

--- src/main/java/racingcar/model/RacingGame.java
+++ src/main/java/racingcar/model/RacingGame.java
@@ -1,16 +1,24 @@
 package racingcar.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class RacingGame {
+    private static final int START_RANDOM_NUMBER = 0;
+    private static final int END_RANDOM_NUMBER = 9;
+
     private List<Car> cars;
     private final int attemptCount;
 
     public RacingGame(List<String> carNames, int attemptCount) {
         this.cars = createCarsFromNames(carNames);
         this.attemptCount = attemptCount;
+    }
+
+    public int createRandomNumber() {
+        return Randoms.pickNumberInRange(START_RANDOM_NUMBER, END_RANDOM_NUMBER);
     }
 
     public List<Car> getCars() {
