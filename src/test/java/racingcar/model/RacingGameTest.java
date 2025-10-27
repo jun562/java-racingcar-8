79dde5248dc72d2d3d11db6d95862ba69f5b7a5b
jun562 <capboy290@naver.com>
2025-10-27 17:27:25 +0900

    test(unit): 자동차 경주 게임 우승자 선정 단위 테스트 코드 작성

--- src/test/java/racingcar/model/RacingGameTest.java
+++ src/test/java/racingcar/model/RacingGameTest.java
@@ -1,5 +1,6 @@
 package racingcar.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
@@ -24,6 +25,36 @@ class RacingGameTest {
             assertEquals(0, currentCar.getCurrentDistance());
         }
 
+    }
+
+    @Test
+    @DisplayName("단독_우승자_선정")
+    void findSingleWinner() {
+        RacingGame game = new RacingGame(List.of("pobi", "car", "jun"), 1);
+        List<Car> cars = game.getCars();
+
+        cars.get(0).move(4); // pobi
+        cars.get(1).move(3); // car
+        cars.get(2).move(3); // jun
+
+        List<String> winners = game.findWinners();
+
+        assertThat(winners).containsOnly("pobi");
+    }
+
+    @Test
+    @DisplayName("공동_우승자_선정")
+    void findMultipleWinners() {
+        RacingGame game = new RacingGame(List.of("pobi", "car", "jun"), 1);
+        List<Car> cars = game.getCars();
+
+        cars.get(0).move(4); // pobi
+        cars.get(1).move(4); // car
+        cars.get(2).move(3); // jun
+
+        List<String> winners = game.findWinners();
+
+        assertThat(winners).containsOnly("pobi", "car");
     }
 
 }
