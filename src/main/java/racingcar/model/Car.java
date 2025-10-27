6f5953c3dfd1e60651721384edc2cd05108c86d9
jun562 <capboy290@naver.com>
2025-10-27 16:33:56 +0900

    feat(car): 무작위 값에 따른 자동차 이동 로직 구현

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,12 +1,20 @@
 package racingcar.model;
 
 public class Car {
+    private static final int MOVE_THRESHOLD = 4;
+
     private final String name;
     private int distance;
 
     public Car(String name) {
         this.name = name;
         this.distance = 0;
+    }
+
+    public void move(int number) {
+        if (number >= MOVE_THRESHOLD) {
+            this.distance++;
+        }
     }
 
     public String getCarName() {
