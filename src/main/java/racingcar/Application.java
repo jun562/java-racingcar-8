393b70c5cff44eb639546240d0f3306414b9cced
jun562 <capboy290@naver.com>
2025-10-27 20:25:31 +0900

    feat(main): 메인 로직 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,25 @@
 package racingcar;
 
+import racingcar.controller.InputController;
+import racingcar.controller.RacingGameController;
+import racingcar.model.AttemptCountValidator;
+import racingcar.model.CarNameValidator;
+import racingcar.model.Parser;
+import racingcar.model.RacingGame;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Parser paser = new Parser();
+        CarNameValidator carNameValidator = new CarNameValidator();
+        AttemptCountValidator attemptCountValidator = new AttemptCountValidator();
+
+        // 컨트롤러 실행
+        InputController inputController = new InputController(paser, carNameValidator, attemptCountValidator);
+        RacingGame racingGame = inputController.getRacingGame();
+        RacingGameController racingGameController = new RacingGameController();
+
+        // 게임 진행
+        racingGameController.runGame(racingGame);
     }
 }
