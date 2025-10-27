c03c0de15dcc78d329a87b4e36c942154a5c3141
jun562 <capboy290@naver.com>
2025-10-27 13:08:45 +0900

    test(parser): 빈 문자열을 처리하도록 파라미터 테스트 수정

--- src/test/java/racingcar/model/ParserTest.java
+++ src/test/java/racingcar/model/ParserTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class ParserTest {
     private Parser parser;
@@ -91,7 +92,7 @@ class ParserTest {
     }
 
     @ParameterizedTest
-    @CsvSource({
+    @ValueSource(strings = {
             "/",
             "1.1",
             "",
